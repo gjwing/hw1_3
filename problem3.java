b1e4ed7bf0aea3e42399217f0b60fb78d7c1bf08
Greg Wing <Greg Wing>
2016-07-21 21:28:09 -0700

    initialhw4

--- problem3.java
+++ problem3.java
@@ -1,10 +1,19 @@
 package homework1;
 
+import java.util.Scanner;
+
 public class problem3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner in = new Scanner(System.in);
+System.out.print("Enter an Integer");
+int x = in.nextInt();
+System.out.print("Enter another Integer");
+int y = in.nextInt();
+int z = x+y;
+System.out.println(z);
+in.close();
 	}
 
 }
